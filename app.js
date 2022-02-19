7baff171d4ea3eed13a59e2552004a74863fd185
samet <smtartul@gmail.com>
2022-02-19 18:51:47 +0300

    public dir added & static page codes applied

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const path=require('path')
 
 const app = express();
 
+// middlewares
+// const myLogger=(req,res,next)=>{
+//   console.log("Middleware log 1");
+//   next();
+// }
+
+// app.use(myLogger)
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
 
-    const photo={
-        id:1,
-        name:"Photo Name",
-        description:"Photo Description"
-    }
-  res.send(photo);
+  //   const photo={
+  //       id:1,
+  //       name:"Photo Name",
+  //       description:"Photo Description"
+  //   }
+  // res.send(photo);
+  res.sendFile(path.resolve(__dirname,'temp/index.html'))
 });
 
 const port = 4000;
