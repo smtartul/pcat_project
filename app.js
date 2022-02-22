712fea383f02266b095f78bbeafe502b4240d51a
Samet ARTUL <smtartul@gmail.com>
2022-02-23 00:22:00 +0300

    views edited

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path=require('path')
+const ejs=require('ejs')
+const path = require('path');
 
 const app = express();
-
+app.set("view engine","ejs")
 // middlewares
 // const myLogger=(req,res,next)=>{
 //   console.log("Middleware log 1");
@@ -10,18 +11,25 @@ const app = express();
 // }
 
 // app.use(myLogger)
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
   //   const photo={
   //       id:1,
   //       name:"Photo Name",
   //       description:"Photo Description"
   //   }
   // res.send(photo);
-  res.sendFile(path.resolve(__dirname,'temp/index.html'))
+  // res.sendFile(path.resolve(__dirname,'temp/index.html'))
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
 
 const port = 4000;
 app.listen(port, () => {
